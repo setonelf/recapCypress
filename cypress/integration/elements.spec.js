b898f20f4274197925441a3b8dd61fdd3980960c
setonelf <freitas.war@gmail.com>
2025-07-09 11:18:04 -0400

    Adicionando teste para lidar com texto

---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Trabalhando com elementos basicos...', ()=> {
+    it('Texto', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.get('body').should('contain', 'Cuidado')
+        cy.get('span').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+    })
+})
