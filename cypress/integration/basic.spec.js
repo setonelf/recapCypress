d32b135fec0efcd6dfedc04c8f3316af8b6a1475
setonelf <freitas.war@gmail.com>
2025-07-09 11:05:01 -0400

    Adicionando os testes básicos de Cypress

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+describe('Basicos do Cypress', () => {
+    it.only('Deve visitar uma página e fazer uma assertiva no título', ()=>{
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        
+        // const title = cy.title()
+        // console.log(title)
+
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo').debug()
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
+    })
+
+    it('Deve encontrar e interagir com um elemento', ()=>{
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+    })
+})
