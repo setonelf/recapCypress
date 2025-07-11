3ec5680483eb11afeec6d2cc04843306c49af72b
setonelf <freitas.war@gmail.com>
2025-07-11 08:53:39 -0400

    Adicionado arquivos nao sincronizados

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -8,4 +8,20 @@ describe('Trabalhando com elementos basicos...', ()=> {
         cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
+
+    it.only('Links', ()=> {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.get('[href="#"]')
+            .click()
+        cy.get('#resultado')
+            .should('have.text', 'Voltou!')
+
+        cy.reload()
+        cy.get('#resultado')
+            .should('have.not.text', 'Voltou!')
+        cy.contains('Voltar')
+            .click()
+        cy.get('#resultado')
+            .should('have.text', 'Voltou!')
+    })
 })
