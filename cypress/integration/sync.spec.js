951aed29e3e3e8ffbf69a07502e2c66457218e8e
setonelf <freitas.war@gmail.com>
2025-07-23 09:13:49 -0400

    Adicionando teste com retentativas

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -10,11 +10,19 @@ describe('Esperas', ()=>{
         cy.reload()
     })
 
-    it.only('Deve aguardar elemento estar disponível', ()=>{
+    it('Deve aguardar elemento estar disponível', ()=>{
         cy.get('#novoCampo').should('not.exist')
         cy.get('#buttonDelay').click()
         cy.get('#novoCampo').should('not.exist')
         cy.get('#novoCampo').should('exist')
         cy.get('#novoCampo').type('funciona')
+    })
+
+    it.only('Deve fazer retentativas', ()=>{
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('funciona')
+            //.should('not.exist')
     })
 })
